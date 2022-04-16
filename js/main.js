9bce0a1c32b4d8662ef9f31f2cabf18bf55707a0
yanatsb <cybina96@mail.ru>
2022-04-16 21:08:36 +0300

    add lightGallery

--- js/main.js
+++ js/main.js
@@ -4,20 +4,23 @@ menuBtn.addEventListener('click', function(){
 	menu.classList.toggle('active');
 });
 
-  document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
 
-    const elements = document.querySelectorAll('[data-mask="phone"]') 
-    if (!elements) return 
-    const phoneOptions = { 
-      mask: '+{7}-000-000-00-00'
-    }
-    elements.forEach(el => { 
-      IMask(el, phoneOptions)
-    });
-  
+  const elements = document.querySelectorAll('[data-mask="phone"]') 
+  if (!elements) return 
+  const phoneOptions = { 
+    mask: '+{7}-000-000-00-00'
+  }
+  elements.forEach(el => { 
+    IMask(el, phoneOptions)
   });
 
-  function scrollToTop() {
-    window.scroll({ top: 0, left: 0, behavior: 'smooth' });
-  };
-  
+});
+
+
+function scrollToTop() {
+  window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+};
+
+lightGallery(document.getElementById('ul-li'));
+
