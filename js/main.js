ed65c4fe28ee0c203918cc6c4f12ef7684397992
yanatsb <cybina96@mail.ru>
2022-04-16 15:47:00 +0300

    add section, imask

--- js/main.js
+++ js/main.js
@@ -3,3 +3,17 @@ let menu = document.querySelector('.header__nav');
 menuBtn.addEventListener('click', function(){
 	menu.classList.toggle('active');
 });
+
+  document.addEventListener('DOMContentLoaded', () => {
+
+    const elements = document.querySelectorAll('[data-mask="phone"]') 
+    if (!elements) return 
+    const phoneOptions = { 
+      mask: '+{7}-000-000-00-00'
+    }
+    elements.forEach(el => { 
+      IMask(el, phoneOptions)
+    });
+  
+  });
+  
