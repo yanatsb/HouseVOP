7542f87e050ad8c70e77a50dcd13984455f6d041
yanatsb <cybina96@mail.ru>
2022-04-16 14:17:50 +0300

    add adaptive menu

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let menuBtn = document.querySelector('.header__burger');
+let menu = document.querySelector('.header__nav');
+menuBtn.addEventListener('click', function(){
+	menu.classList.toggle('active');
+});
